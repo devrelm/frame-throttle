a92580f99ae75331103af6e50a4290716aefe058
Mike Deverell <mike@pelotoncycle.com>
2016-09-26 15:29:12 -0400

    move sinon type declaration to npm dependency

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,5 +2,4 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="jsdom/jsdom.d.ts" />
 /// <reference path="node/node.d.ts" />
-/// <reference path="sinon/sinon.d.ts" />
 /// <reference path="tape/tape.d.ts" />
