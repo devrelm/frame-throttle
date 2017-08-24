b3169ca0d3b9781201de098fa12e9545f4899bd7
Mike Deverell <mike@pelotoncycle.com>
2017-08-23 22:15:29 -0400

    refactor: pull wrapperFactory out from `throttle`

--- src/frame-throttle.ts
+++ src/frame-throttle.ts
@@ -4,42 +4,42 @@ export type Cancellable<T extends Function>
         cancel(): void;
     }
 
-export const throttle = <T extends Function>(callback: T): Cancellable<T> => {
-    const wrapperFactory = function (wrapperContext: any) {
-        const resetCancelToken = () => {
-            wrapperContext.cancelToken = false;
-        };
-
-        const wrapper = (cbThis: any, cb: T, ...args: any[]) => {
-            wrapperContext.callbackThis = cbThis;
-            wrapperContext.args = args;
-
-            if (wrapperContext.cancelToken) {
-                return;
-            }
-
-            if ('requestAnimationFrame' in window) {
-                wrapperContext.cancelToken = window.requestAnimationFrame(() => {
-                    cb.apply(wrapperContext.callbackThis, wrapperContext.args);
-                    resetCancelToken();
-                });
-            } else {
-                cb.apply(wrapperContext.callbackThis, wrapperContext.args);
-                wrapperContext.cancelToken = window.setTimeout(resetCancelToken, 1000 / 60); // 60 fps
-            }
-        };
-
-        (wrapper as Cancellable<typeof wrapper>).cancel = () => {
-            if ('requestAnimationFrame' in window) {
-                window.cancelAnimationFrame(wrapperContext.cancelToken);
-            }
-            window.clearTimeout(wrapperContext.cancelToken);
-            resetCancelToken();
-        };
-
-        return wrapper as Cancellable<typeof wrapper>;
+const wrapperFactory = function (wrapperContext: any) {
+    const resetCancelToken = () => {
+        wrapperContext.cancelToken = false;
     };
 
+    const wrapper = <T extends Function>(cbThis: any, cb: T, ...args: any[]) => {
+        wrapperContext.callbackThis = cbThis;
+        wrapperContext.args = args;
+
+        if (wrapperContext.cancelToken) {
+            return;
+        }
+
+        if ('requestAnimationFrame' in window) {
+            wrapperContext.cancelToken = window.requestAnimationFrame(() => {
+                cb.apply(wrapperContext.callbackThis, wrapperContext.args);
+                resetCancelToken();
+            });
+        } else {
+            cb.apply(wrapperContext.callbackThis, wrapperContext.args);
+            wrapperContext.cancelToken = window.setTimeout(resetCancelToken, 1000 / 60); // 60 fps
+        }
+    };
+
+    (wrapper as Cancellable<typeof wrapper>).cancel = () => {
+        if ('requestAnimationFrame' in window) {
+            window.cancelAnimationFrame(wrapperContext.cancelToken);
+        }
+        window.clearTimeout(wrapperContext.cancelToken);
+        resetCancelToken();
+    };
+
+    return wrapper as Cancellable<typeof wrapper>;
+};
+
+export const throttle = <T extends Function>(callback: T): Cancellable<T> => {
     const wrapper = wrapperFactory({});
     const throttledCallback = function (...args: any[]) {
         wrapper(this, callback, ...args);
