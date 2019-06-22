77c8afe418b4025d3999e7e60fdac55e03cd8615
Mike Deverell <mike@onepeloton.com>
2019-06-21 23:17:31 -0400

    prettier index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,62 @@
-export type Cancellable<T extends Function>
-    = T
-    & {
-        /**
-         * Cancel the next scheduled invocation of the callback.
-         */
-        cancel(): void;
-    }
+export type Cancellable<T extends Function> = T & {
+  /**
+   * Cancel the next scheduled invocation of the callback.
+   */
+  cancel(): void;
+};
 
 type WindowWithoutRAF = Omit<Window, 'requestAnimationFrame'>;
 
-const wrapperFactory = function (wrapperContext: any) {
-    const resetCancelToken = () => {
-        wrapperContext.cancelToken = false;
-    };
+const wrapperFactory = function(wrapperContext: any) {
+  const resetCancelToken = () => {
+    wrapperContext.cancelToken = false;
+  };
 
-    const wrapper = <T extends Function>(cbThis: any, cb: T, ...args: any[]) => {
-        wrapperContext.callbackThis = cbThis;
-        wrapperContext.args = args;
+  const wrapper = <T extends Function>(cbThis: any, cb: T, ...args: any[]) => {
+    wrapperContext.callbackThis = cbThis;
+    wrapperContext.args = args;
 
-        if (wrapperContext.cancelToken) {
-            return;
-        }
+    if (wrapperContext.cancelToken) {
+      return;
+    }
 
-        if ('requestAnimationFrame' in window) {
-            wrapperContext.cancelToken = window.requestAnimationFrame(() => {
-                cb.apply(wrapperContext.callbackThis, wrapperContext.args);
-                resetCancelToken();
-            });
-        } else {
-            cb.apply(wrapperContext.callbackThis, wrapperContext.args);
-            wrapperContext.cancelToken = (window as WindowWithoutRAF).setTimeout(resetCancelToken, 1000 / 60); // 60 fps
-        }
-    };
-
-    (wrapper as Cancellable<typeof wrapper>).cancel = () => {
-        if ('requestAnimationFrame' in window) {
-            window.cancelAnimationFrame(wrapperContext.cancelToken);
-        }
-        window.clearTimeout(wrapperContext.cancelToken);
+    if ('requestAnimationFrame' in window) {
+      wrapperContext.cancelToken = window.requestAnimationFrame(() => {
+        cb.apply(wrapperContext.callbackThis, wrapperContext.args);
         resetCancelToken();
-    };
+      });
+    } else {
+      cb.apply(wrapperContext.callbackThis, wrapperContext.args);
+      wrapperContext.cancelToken = (window as WindowWithoutRAF).setTimeout(
+        resetCancelToken,
+        1000 / 60
+      ); // 60 fps
+    }
+  };
 
-    return wrapper as Cancellable<typeof wrapper>;
+  (wrapper as Cancellable<typeof wrapper>).cancel = () => {
+    if ('requestAnimationFrame' in window) {
+      window.cancelAnimationFrame(wrapperContext.cancelToken);
+    }
+    window.clearTimeout(wrapperContext.cancelToken);
+    resetCancelToken();
+  };
+
+  return wrapper as Cancellable<typeof wrapper>;
 };
 
-const throttleFactory = function <T extends Function>(callback: T, thisArg?: any, ...argArray: any[]) {
-    const wrapper = wrapperFactory({});
-    const argCount = arguments.length;
-    const throttledCallback = function (...args: any[]) {
-        wrapper(argCount > 1 ? thisArg : this, callback, ...argArray, ...args);
-    } as any as Cancellable<T>;
-    throttledCallback.cancel = () => wrapper.cancel();
-    return throttledCallback;
+const throttleFactory = function<T extends Function>(
+  callback: T,
+  thisArg?: any,
+  ...argArray: any[]
+) {
+  const wrapper = wrapperFactory({});
+  const argCount = arguments.length;
+  const throttledCallback = (function(...args: any[]) {
+    wrapper(argCount > 1 ? thisArg : this, callback, ...argArray, ...args);
+  } as any) as Cancellable<T>;
+  throttledCallback.cancel = () => wrapper.cancel();
+  return throttledCallback;
 };
 
 /**
@@ -62,11 +67,11 @@ const throttleFactory = function <T extends Function>(callback: T, thisArg?: any
  * @param callback the function to be throttled
  */
 export const throttle = <T extends Function>(callback: T): Cancellable<T> => {
-    const throttledCallback = throttleFactory(callback);
+  const throttledCallback = throttleFactory(callback);
 
-    // Override `bind()` to create a new throttled callback, otherwise both
-    // the unbound and bound callbacks will have the same scope.
-    throttledCallback.bind = throttleFactory.bind(null, callback);
+  // Override `bind()` to create a new throttled callback, otherwise both
+  // the unbound and bound callbacks will have the same scope.
+  throttledCallback.bind = throttleFactory.bind(null, callback);
 
-    return throttledCallback;
+  return throttledCallback;
 };
